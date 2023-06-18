625fae72c2bd3f474f537c39a021bf9823b20c4a
CasheeeewNuts <casheeeewnuts+tech@gmail.com>
2023-06-18 21:03:38 +0900

    HTTPヘッダに関するモジュールを追加

---
+++ http/http_header.c
@@ -0,0 +1,36 @@
+//
+// Created by casheeeewnuts on 6/18/23.
+//
+
+#include <glib-2.0/glib.h>
+#include "http_header.h"
+
+void add_header(Header *headers, char *key, char *value);
+
+void del_header(Header *headers, char *key);
+
+void destroy_header(Header *headers);
+
+Header *new_header() {
+    Header *header = calloc(1, sizeof(Header));
+
+    header->table = g_hash_table_new(g_str_hash, g_str_equal);
+    header->add = add_header;
+    header->remove = del_header;
+    header->destroy = destroy_header;
+
+    return header;
+}
+
+void add_header(Header *headers, char *key, char *value) {
+    g_hash_table_insert(headers->table, g_strdup(key), g_strdup(value));
+}
+
+void del_header(Header *headers, char *key) {
+    g_hash_table_remove(headers->table, key);
+}
+
+void destroy_header(Header *headers) {
+    g_hash_table_destroy(headers->table);
+    free(headers);
+}
---
+++ http/http_header.h
@@ -0,0 +1,21 @@
+//
+// Created by casheeeewnuts on 6/18/23.
+//
+
+#ifndef PICO_HTTP_SERVER_HTTP_HEADER_H
+#define PICO_HTTP_SERVER_HTTP_HEADER_H
+
+#include <glib-2.0/glib.h>
+
+typedef struct Header {
+    GHashTable *table;
+
+    void (*add)(struct Header *, char *key, char *value);
+
+    void (*remove)(struct Header *, char *key);
+
+    void (*destroy)(struct Header *);
+} Header;
+
+Header *new_header();
+#endif //PICO_HTTP_SERVER_HTTP_HEADER_H
